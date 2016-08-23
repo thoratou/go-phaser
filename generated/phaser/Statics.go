d5a6af2e90d997203275a54640a5bf983f207400
tt.tranquang <tt.tranquang@gmail.com>
2016-08-23 22:14:54 +0200

    removed unused struct

--- generated/phaser/Statics.go
+++ generated/phaser/Statics.go
@@ -4,16 +4,6 @@ import (
 	"github.com/gopherjs/gopherjs/js"
 )
 
-type PhysicsGlobal struct {
-	*js.Object
-	ARCADE   int `js:"ARCADE"`
-	P2JS     int `js:"P2JS"`
-	NINJA    int `js:"NINJA"`
-	BOX2D    int `js:"BOX2D"`
-	MATTER   int `js:"MATTER"`
-	CHIPMUNK int `js:"CHIPMUNK"`
-}
-
 func GetGlobalConst(key string) int {
 	return js.Global.Get("Phaser").Get(key).Int()
 }
